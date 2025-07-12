54c22cc779f773a2ffb542db6b171a455eec25a7
D...Line <onlyredline+github@gmail.com>
2025-07-12 00:11:02 -0700

    loggerMiddleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import * as express from "express";
 
-const app = express();
+function loggerMiddleware(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) {
+  console.log(`${req.method} ${req.path}`);
+  next();
+}
 
-app.get("/", (req, res) => {
+const app = express();
+app.use(loggerMiddleware);
+
+app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
 
